496f386ce3ad657a8e815ed80f4384b97f66056a
Timok951 <timofey.alekseev@list.ru>
2023-10-04 14:47:53 +0300

    new code

--- calculator.py
+++ calculator.py
@@ -6,135 +6,75 @@ number2 = 0
 read = ""
 
 while True:
-	print("1-сложение\n2-вычитание\n3-умножение\n4-деление\n5-возведение в степень\n6-квадратный корень\n7-факториал\n8-синус\n9-косинус\n")
-	read=input("введите номер операции ")
-	operation = int(read)
-	match operation:
-		case 1: 
-				read = input("введите первое число ")
-				try :
+	try: 
+		print("1-сложение\n2-вычитание\n3-умножение\n4-деление\n5-возведение в степень\n6-квадратный корень\n7-факториал\n8-синус\n9-косинус\n")
+		read=input("введите номер операции ")
+		operation = int(read)
+		match operation:
+			case 1: 
+					read = input("введите первое число ")
 					number1 = int(read)
-				except: 
-					print("введите число")
-					break
-
-				try :
 					read = input("введите второе число ")
-					number2 = int(read)
-					
-				except: 
-					print("введите число")
-					break
-				print(number1 + number2)
-		case 2: 
-				read = input("введите первое число ")
-				try :
-					number1 = int(read)
-				except: 
-					print("введите число ")
-					break
-
-				try :
-					read = input("введите второе число ")
-					number2 = int(read)
-					
-				except: 
-					print("введите число")
-					break
-				print(number1 - number2)
-		case 3: 
-				read = input("введите первое число ")
-				try :
-					number1 = int(read)
-				except: 
-					print("введите число ")
-					break
-
-				try :
-					read = input("введите второе число ")
-					number2 = int(read)
-					
-				except: 
-					print("введите число ")
-					break
-				print(number1 * number2)
-		case 4: 
-				read = input("введите первое число ")
-				try :
-					number1 = int(read)
-				except: 
-					print("введите число ")
-					break
-
-				try :
-					read = input("введите второе число ")
-					number2 = int(read)
-					
-				except: 
-					print("введите число ")
-					break
-				print(number1 / number2)
-
-		case 5: 
-				read = input("введите первое число ")
-				try :
-					number1 = int(read)
-				except: 
-					print("введите число ")
-					break
-
-				try :
-					read = input("введите степень ")
 					number2 = int(read)	
-				except: 
+					print(number1 + number2)
+			case 2: 
+					read = input("введите первое число ")
+					number1 = int(read)
+					read = input("введите второе число ")
+					number2 = int(read)
+					print(number1 - number2)
+			case 3: 
+					read = input("введите первое число ")
+					number1 = int(read)
+					read = input("введите второе число ")
+					number2 = int(read)
+					print(number1 * number2)
+			case 4: 
+					read = input("введите первое число ")
+					number1 = int(read)
 					print("введите число ")
-					break
+					read = input("введите второе число ")
+					number2 = int(read)
+					print(number1 / number2)
 
-				num = number1
-				i = 1
-				while i != number2:
-					number1 = number1 * num
-					i = i + 1
-					break
-				print (number1)
-		case 6: 
-				read = input("введите первое число ")
-				try :
+			case 5: 
+					read = input("введите первое число ")
 					number1 = int(read)
-				except: 
-					print("введите число")
-					break
+					print("введите число ")
+					read = input("введите степень ")
 
-				print(math.sqrt(number1))
 
-		case 7: 
-				read = input("введите первое число ")
-				try :
+
+
+					num = number1
+					i = 1
+					while i != number2:
+						number1 = number1 * num
+						i = i + 1
+					print (number1)
+			case 6: 
+					read = input("введите первое число ")
 					number1 = int(read)
-				except: 
-					print("введите число")
-					break
+					print(math.sqrt(number1))
 
-				print(math.factorial(number1))
-		
-		case 8: 
-				read = input("введите первое число ")
-				try :
+			case 7: 
+					read = input("введите первое число ")
 					number1 = int(read)
-				except: 
 					print("введите число")
-					break
 
-				print(math.sin(number1))
-		case 9: 
-				read = input("введите первое число ")
-				try :
+					print(math.factorial(number1))
+			
+			case 8: 
+					read = input("введите первое число ")
 					number1 = int(read)
-				except: 
 					print("введите число")
-					break
-
-				print(math.cos(number1))
+					print(math.sin(number1))
+			case 9: 
+					read = input("введите первое число ")
+					number1 = int(read)
+					print(math.cos(number1))
+	except:
+			print("Error") 
 
 	if (operation == 10):
 		break 
